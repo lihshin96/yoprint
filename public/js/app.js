3600e2f55e0ad43d90f2f65a5e758d249fdc999f
Shin <lihshin_96@outlook.com>
2023-10-15 20:24:57 +0800

    Update gitignore and websocket config

--- public/js/app.js
+++ public/js/app.js
@@ -5367,8 +5367,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.Pusher = __webpack_require__(/*! pusher-js */ "./node_modules/pusher-js/dist/web/pusher.js");
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: 'pusher',
-  key: "",
-  cluster: "mt1",
+  key: "395c3f9950fb5605244a",
+  cluster: "ap1",
   forceTLS: true
 });
 
